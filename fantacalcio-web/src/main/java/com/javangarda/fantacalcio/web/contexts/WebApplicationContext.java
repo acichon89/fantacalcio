9d5016fc225daa9d42db1d0833d930d37d240ddb
andrzej <acichon89@gmail.com>
2016-02-11 21:36:07 +0100

    Added webapp module

---
+++ fantacalcio-web/src/main/java/com/javangarda/fantacalcio/web/contexts/WebApplicationContext.java
@@ -0,0 +1,28 @@
+package com.javangarda.fantacalcio.web.contexts;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages={"com.javangarda.fantacalcio.web.controllers"})
+public class WebApplicationContext extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+    public InternalResourceViewResolver jspViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/views/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+}
---
+++ fantacalcio-web/src/main/java/com/javangarda/fantacalcio/web/init/FantaCalcioApplicationInitializer.java
@@ -0,0 +1,33 @@
+package com.javangarda.fantacalcio.web.init;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.javangarda.fantacalcio.gamerules.domain.application.PersistenceContext;
+import com.javangarda.fantacalcio.web.contexts.WebApplicationContext;
+
+public class FantaCalcioApplicationInitializer implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+		appContext.register(PersistenceContext.class);
+		
+	    servletContext.addListener(new ContextLoaderListener(appContext));
+	    
+	    AnnotationConfigWebApplicationContext dispatcherServlet = new AnnotationConfigWebApplicationContext();
+	    dispatcherServlet.register(WebApplicationContext.class);
+	    
+	    ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(dispatcherServlet));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+         
+	}
+
+}
