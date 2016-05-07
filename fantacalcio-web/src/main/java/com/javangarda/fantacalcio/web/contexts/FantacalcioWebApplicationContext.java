6dfa53eeb457e5b82f704f7fbe69edc5e7cd6b29
andrzej <acichon89@gmail.com>
2016-05-07 11:34:46 +0200

    web url moved to env property clean up webapp context

--- fantacalcio-web/src/main/java/com/javangarda/fantacalcio/web/contexts/FantacalcioWebApplicationContext.java
+++ fantacalcio-web/src/main/java/com/javangarda/fantacalcio/web/contexts/FantacalcioWebApplicationContext.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -17,6 +18,11 @@ public class FantacalcioWebApplicationContext extends WebMvcConfigurerAdapter {
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
 
 	@Bean
     public InternalResourceViewResolver jspViewResolver() {
