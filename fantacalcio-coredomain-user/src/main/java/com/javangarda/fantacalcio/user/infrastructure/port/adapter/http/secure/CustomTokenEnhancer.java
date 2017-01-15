68fe4fd2306074db616b28dd64d024eb68256fd2
acichon89@gmail.com <acichon89@gmail.com>
2017-01-15 23:00:22 +0100

    CustomTokenENhancer fix

--- fantacalcio-coredomain-user/src/main/java/com/javangarda/fantacalcio/user/infrastructure/port/adapter/http/secure/CustomTokenEnhancer.java
+++ fantacalcio-coredomain-user/src/main/java/com/javangarda/fantacalcio/user/infrastructure/port/adapter/http/secure/CustomTokenEnhancer.java
@@ -3,25 +3,20 @@ package com.javangarda.fantacalcio.user.infrastructure.port.adapter.http.secure;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.stereotype.Component;
 
-import com.javangarda.fantacalcio.user.application.internal.RandomStringGenerator;
-
 @Component
 public class CustomTokenEnhancer implements TokenEnhancer {
-	
-	@Autowired
-	private RandomStringGenerator randomStringGenerator;
 	
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         final Map<String, Object> additionalInfo = new HashMap<>();
-        additionalInfo.put("organization", authentication.getName() + randomStringGenerator.generateRandomAlphaNumericString(10));
+        additionalInfo.put("organization", authentication.getName() + RandomStringUtils.randomAlphanumeric(10));
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
         return accessToken;
     }
