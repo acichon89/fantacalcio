d5aa5e6c437535f8524532c89bfe7c2aeafc9742
acichon89@gmail.com <acichon89@gmail.com>
2017-03-04 22:23:30 +0100

    Refactored user domain, added validation constraint (fix2)

--- fantacalcio-auth-server/src/main/java/com/javangarda/fantacalcio/authserver/application/saga/MessageHandler.java
+++ fantacalcio-auth-server/src/main/java/com/javangarda/fantacalcio/authserver/application/saga/MessageHandler.java
@@ -1,9 +1,7 @@
 package com.javangarda.fantacalcio.authserver.application.saga;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.integration.annotation.MessageEndpoint;
-import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,7 +11,6 @@ public class MessageHandler {
     @Autowired
     private TokenStore tokenStore;
 
-    @ServiceActivator(inputChannel = Sink.INPUT)
     @Transactional
     public void foo(String email){
         tokenStore.removeAccessToken(tokenStore.readAccessToken("0c1814b7410673b1a290cca9a5b5a558"));
